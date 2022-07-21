701ab7c7c2565572fa5fe332fd7a086d65bfc01e
Gustavo de Matos Parizi <gusparizi@gmail.com>
2022-07-21 12:44:55 -0300

    commiting implementation

---
+++ src/main/java/com/mecrudtest/mercadoeletronicotest/MercadoEletronicoTestApplication.java
@@ -0,0 +1,13 @@
+package com.mecrudtest.mercadoeletronicotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoEletronicoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoEletronicoTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mecrudtest/mercadoeletronicotest/contacts/models/Contact.java
@@ -0,0 +1,76 @@
+package com.mecrudtest.mercadoeletronicotest.contacts.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String mail;
+
+    private String urlAvatar;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getUrlAvatar() {
+        return urlAvatar;
+    }
+
+    public void setUrlAvatar(String urlAvatar) {
+        this.urlAvatar = urlAvatar;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contact)) return false;
+        Contact contact = (Contact) o;
+        return getId() == contact.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
